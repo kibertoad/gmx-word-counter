85092e19d0914dc2c6eb0e44cf17f92ec881ffac
Igor Savin <iselwin@gmail.com>
2023-08-20 02:13:46 +0300

    Fix dravidian languages, remove unnecessary parts of the regex

--- lib/wordCounter.ts
+++ lib/wordCounter.ts
@@ -6,9 +6,16 @@ import {
 } from './logographicCounter'
 import { countCharacters } from './characterCounter'
 
-// RegEx taken from https://github.com/regexhq/word-regex by Jon Schlinkert (MIT license)
+// RegEx taken from https://github.com/regexhq/word-regex by Jon Schlinkert (MIT license), with CJK and Telugu removed (they are handled separately)
 const NON_LOGOGRAPHIC_LANGUAGE_REGEX =
-  /[a-zA-Z0-9_'\u0392-\u03c9\u0400-\u04FF\u0027]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af\u0400-\u04FF]+|[\u00E4\u00C4\u00E5\u00C5\u00F6\u00D6]+|[\u0531-\u0556\u0561-\u0586\u0559\u055A\u055B]+|\w+/g
+  /[\u0392-\u03c9\u0400-\u04FF\u0027\u00E4\u00C4\u00E5\u00C5\u00F6\u00D6\u0531-\u0556\u0561-\u0586\u0559\u055A\u055B\w]+/g
+
+const localeRegexMap: Record<string, RegExp> = {
+  ta: /[\u0B80-\u0BFF]+/g,
+  te: /[\u0C00-\u0C7F\-]+/g,
+  kn: /[\u0C80-\u0CFF]+/g,
+  ml: /[\u0D00-\u0D7F]+(?:[\u200C\u200D][\u0D00-\u0D7F]+)*/g,
+}
 
 function countWordsLogographic(segment: string, locale: LocaleId) {
   const factor = getCharacterCountFactor(locale)
@@ -30,7 +37,10 @@ export function countWords(segment: string, locale: string) {
     return 0
   }
 
-  const matches = segment.match(NON_LOGOGRAPHIC_LANGUAGE_REGEX)
+  // Let's see if we have locale-specific regex
+  const regex = localeRegexMap[locale]
+
+  const matches = regex ? segment.match(regex) : segment.match(NON_LOGOGRAPHIC_LANGUAGE_REGEX)
 
   return matches?.length ?? 0
 }
